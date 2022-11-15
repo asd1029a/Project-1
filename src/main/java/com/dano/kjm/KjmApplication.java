f00099f76319707c72b88f18509f906b2839d575
asd1029a <xxx1029x@gmail.com>
2022-11-15 20:08:09 +0900

    init

---
+++ src/main/java/com/dano/kjm/KjmApplication.java
@@ -0,0 +1,13 @@
+package com.dano.kjm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KjmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KjmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dano/kjm/KjmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dano.kjm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KjmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
